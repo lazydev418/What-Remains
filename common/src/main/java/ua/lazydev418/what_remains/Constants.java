a068db6eee7d1825bbdbf70f98ac3f73c6e62402
Vadym <61838855+lazydev418@users.noreply.github.com>
2026-07-22 14:14:45 +0300

    Setup structure.

---
+++ common/src/main/java/ua/lazydev418/what_remains/Constants.java
@@ -0,0 +1,11 @@
+package ua.lazydev418.what_remains;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "what_remains";
+    public static final String MOD_NAME = "What Remains";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ neoforge/src/main/java/ua/lazydev418/what_remains/WhatRemains.java
@@ -0,0 +1,10 @@
+package ua.lazydev418.what_remains;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class WhatRemains {
+    public WhatRemains(IEventBus eventBus) {
+    }
+}
